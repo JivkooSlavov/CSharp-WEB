a0c634ca254ef91710a14a46e4c17008564d8ba8
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-10-25 21:59:46 +0300

    Add files via upload

---
+++ ASP.NET Fundamentals/03.Text Splitter App/3.Text Splitter App/3.Text Splitter App/Models/TextViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3.Text_Splitter_App.Models
+{
+    public class TextViewModel
+    {
+        [Required]
+        [StringLength (30, MinimumLength = 4)]
+        public string Text { get; set; } = null!;
+
+        public string SplitText { get; set; } = null!;
+    }
+}
